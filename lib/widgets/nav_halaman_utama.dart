5b143acbb3190a36e70cea41ed7be452f1507812
Rafid Muhammad <rafid.muhammad@gmail.com>
2023-01-18 20:40:37 +0700

    create bacward navigation to main page

---
+++ lib/widgets/nav_halaman_utama.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:ta_schizo/shared/theme.dart';
+
+class BackToMain extends StatelessWidget {
+  const BackToMain({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.pop(context);
+      },
+      child: Container(
+        margin: const EdgeInsets.only(top: 15),
+        child: Row(children: [
+          Icon(
+            Icons.arrow_back,
+            color: kPrimaryColor2,
+          ),
+          const SizedBox(
+            width: 5,
+          ),
+          Text(
+            "Halaman Utama",
+            style: mainTextStyle.copyWith(fontSize: 15, fontWeight: medium),
+          ),
+        ]),
+      ),
+    );
+  }
+}
